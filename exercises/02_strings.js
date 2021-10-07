066901c36e78a9e8e501a35da18200360e2fff30
Sabrina <pereira.tsabrina@gmail.com>
2021-10-07 10:39:14 -0300

    2-b

--- exercises/02_strings.js
+++ exercises/02_strings.js
@@ -5,3 +5,11 @@ var a="sabrina pereira";
 var b=a.toUpperCase();
 
 console.log(b);
+
+/* b) Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los primeros 5
+caracteres guardando el resultado en una nueva variable (utilizar substring).*/
+
+var a="Sabrina Pereira";
+var b= a.substring(0,5);
+
+console.log(b);
