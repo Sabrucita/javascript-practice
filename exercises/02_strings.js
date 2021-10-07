99d1b5b562dd2c3d6b160799bcbaf52f3c4c7be7
Sabrina <pereira.tsabrina@gmail.com>
2021-10-07 10:44:18 -0300

    2-c

--- exercises/02_strings.js
+++ exercises/02_strings.js
@@ -13,3 +13,11 @@ var a="Sabrina Pereira";
 var b= a.substring(0,5);
 
 console.log(b);
+
+/* c) Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los últimos 3
+caracteres guardando el resultado en una nueva variable (utilizar substring).*/
+
+var a="Sabrina Pereira";
+var b= a.substring(a.length - 3);
+
+console.log(b);
