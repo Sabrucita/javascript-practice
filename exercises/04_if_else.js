6746dd71e99d7a2aafbe1406b0daf45765477183
Sabrina <pereira.tsabrina@gmail.com>
2021-10-07 16:38:10 -0300

    4-a

--- exercises/04_if_else.js
+++ exercises/04_if_else.js
@@ -1,4 +1,4 @@
-/*Crear un número aleatorio entre 0 y 1 utilizando la función Math.random(), si el valor es mayor o igual que 0,5
+/* a) Crear un número aleatorio entre 0 y 1 utilizando la función Math.random(), si el valor es mayor o igual que 0,5
 mostrar una alerta con el mensaje “Greater than 0,5” y sino un alerta con el mensaje “Lower than 0,5”.*/
 
 var number = Math.random();
