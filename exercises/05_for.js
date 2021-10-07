d0f7c47ddda3f67d78ae8d16cc9d93294c2282c2
Sabrina <pereira.tsabrina@gmail.com>
2021-10-07 17:37:06 -0300

    5-a

---
+++ exercises/05_for.js
@@ -0,0 +1,8 @@
+/* a) Crear un array que contenga 5 palabras y recorrer dicho array utilizando un bucle for de JavaScript para mostrar
+una alerta utilizando cada una de las palabras.*/
+
+var word = ["Telephone", "Orange", "Book", "Banana", "Bottle"];
+
+for (let i = 0; i < word.length; i++) {
+    alert(word[i]);
+}
