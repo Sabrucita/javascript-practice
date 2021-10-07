060583f061cc9d0878f6c9660cc00598df2ef4ab
Sabrina <pereira.tsabrina@gmail.com>
2021-10-07 10:35:41 -0300

    2-a

--- exercises/02_strings.js
+++ exercises/02_strings.js
@@ -1,7 +1,7 @@
 /* a) Crear una variable de tipo string con al menos 10 caracteres y convertir todo el texto en mayúscula
 (utilizar toUpperCase).*/
 
-a="sabrina pereira";
-b=a.toUpperCase();
+var a="sabrina pereira";
+var b=a.toUpperCase();
 
 console.log(b);
