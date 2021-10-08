efa27ba8f40fd8d4192fb20dd6d2446bfeab3fae
Sabrina <pereira.tsabrina@gmail.com>
2021-10-08 09:32:07 -0300

    5-b

--- exercises/05_for.js
+++ exercises/05_for.js
@@ -1,8 +1,16 @@
 /* a) Crear un array que contenga 5 palabras y recorrer dicho array utilizando un bucle for de JavaScript para mostrar
 una alerta utilizando cada una de las palabras.*/
 
-var word = ["Telephone", "Orange", "Book", "Banana", "Bottle"];
+var word = ["telephone", "orange", "book", "banana", "bottle"];
 
 for (let i = 0; i < word.length; i++) {
     alert(word[i]);
 }
+
+/*Al array anterior convertir la primera letra de cada palabra en mayúscula y mostrar una alerta por cada palabra
+modificada.*/
+
+for (let i = 0; i < word.length; i++) {
+    alert(word[i].substring(0,1).toUpperCase()+word[i].substring(1).toLowerCase());
+}
+
