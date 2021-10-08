98f96dea31335862b91162389bdc407a16935269
Sabrina <pereira.tsabrina@gmail.com>
2021-10-08 10:28:33 -0300

    5-c

--- exercises/05_for.js
+++ exercises/05_for.js
@@ -7,10 +7,20 @@ for (let i = 0; i < word.length; i++) {
     alert(word[i]);
 }
 
-/*Al array anterior convertir la primera letra de cada palabra en mayúscula y mostrar una alerta por cada palabra
+/* b) Al array anterior convertir la primera letra de cada palabra en mayúscula y mostrar una alerta por cada palabra
 modificada.*/
 
 for (let i = 0; i < word.length; i++) {
     alert(word[i].substring(0,1).toUpperCase()+word[i].substring(1).toLowerCase());
 }
 
+/* c) Crear una variable llamada “sentence” que tenga un string vacío, luego al array del punto a) recorrerlo con un
+bucle for para ir guardando cada palabra dentro de la variable sentence. Al final mostrar una única alerta con la cadena
+completa.*/
+
+sentence="";
+
+for (let i = 0; i < word.length; i++) {
+    sentence += word[i] + ", ";
+}
+alert(sentence);
