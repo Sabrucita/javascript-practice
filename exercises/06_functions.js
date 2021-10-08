8752e54ba3b2e02255342bd636b689625a36af4f
Sabrina <pereira.tsabrina@gmail.com>
2021-10-08 11:24:14 -0300

    6-a

---
+++ exercises/06_functions.js
@@ -0,0 +1,9 @@
+/* a) Crear una función suma que reciba dos valores numéricos y retorne el resultado. Ejecutar la función y guardar el
+resultado en una variable, mostrando el valor de dicha variable en la consola del navegador.*/
+
+function addition(a,b) {
+    var results= a+b;
+    console.log(results);
+}
+
+addition(10,3);
